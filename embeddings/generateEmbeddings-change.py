4c76c6a4ebf52f0b36b88e9a25128a8771b1546e
yishangru <yishangru@126.com>
2019-11-19 11:35:07 +0800

    generate new cluster image

--- embeddings/generateEmbeddings-change.py
+++ embeddings/generateEmbeddings-change.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 
 # standardize the data
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import scale
 
 # for dimensionality reduction
 from sklearn.decomposition import PCA
@@ -25,8 +25,6 @@ import matplotlib.pyplot as plt
 # specify whether to use PCA (pca), TSNE (tsne), or MDS (mds)
 mode = "pca"
 
-n_clusters = 1
-
 # set the seed so results are reproducible
 sd = np.random.randint(0,9999999)
 #sd = 1527791
@@ -36,23 +34,27 @@ print("Seed: {}".format(sd))
 np.random.seed(sd)
 
 
-
 def main():
     # load the dataset into a dataframe
-    coffeeData = pd.DataFrame(pd.read_csv("../data/coffee/Coffee-clean.csv", encoding="utf-8"))
+    coffeeData = pd.read_csv("../data/coffee/Coffee-clean.csv", encoding="utf-8")
+
+    # add extra attribute
+    flavorProfile = ['flavor', 'aroma', 'aftertaste', 'acidity', 'balance', 'uniformity', 'sweetness', 'cleanCup', 'cupperPoints', 'moisture', 'altitudeMeanMeters']
+    x_data = coffeeData.loc[:, flavorProfile]
+    x_data = x_data.drop(x_data.query('altitudeMeanMeters==-1').index)
 
     # specify which columns make up the flavor profile
-    flavorProfile = ['flavor', 'aroma', 'aftertaste', 'acidity', 'balance', 'uniformity', 'sweetness', 'cleanCup', 'cupperPoints']
 
     results = None
 
+    x_data_std = scale(x_data)
+
     if mode=="pca":
-        #coffeeData[flavorProfile] = StandardScaler().fit_transform(coffeeData[flavorProfile])
-        #print("Standardized the data")
         print("Performing PCA")
         time_start = time.time()
-        pca = PCA(n_components=3)
-        results = pca.fit_transform(coffeeData[flavorProfile].values)
+        pca = PCA(n_components=2)
+        pca.fit(x_data_std)
+        results = pca.transform(x_data_std)
         print("PCA complete; took {} seconds".format(time.time()-time_start))
         print("Explained variance (per PC): {}".format(pca.explained_variance_ratio_))
         plt.title("PCA")
@@ -62,7 +64,7 @@ def main():
         print("Performing t-SNE")
         time_start = time.time()
         tsne = TSNE(n_components=2, verbose=1, perplexity=30, n_iter=5000)
-        results = tsne.fit_transform(coffeeData[flavorProfile].values)
+        results = tsne.fit_transform(x_data_std)
         print("t-SNE complete; took {} seconds".format(time.time()-time_start))
         plt.title("t-SNE")
 
@@ -71,39 +73,30 @@ def main():
         print("Performing MDS")
         time_start = time.time()
         embedding = MDS(n_components=2, random_state=sd, verbose=1, max_iter=400, metric=True)
-        results = embedding.fit_transform(coffeeData[flavorProfile])
+        results = embedding.fit_transform(x_data_std)
         print("MDS complete; took {} seconds".format(time.time()-time_start))
         plt.title("Similarity space (distance denotes similarity)")
 
-        # store the results in a new dataframe, add appropriate coffee ID to each row
-        coffeeData['mdsX'] = results[:,0]
-        coffeeData['mdsY'] = results[:,1]
-
         #print(mds_coffee.head())
-        
-        # print the results to a new csv
-        coffeeData.to_csv("../data/coffee/coffee-clean.csv", index=False)
-
-    k_means_parameter = {"n_clusters": [i + 1 for i in range(1, 10)]}
-    k_means = KMeans(random_state=sd)
-    k_means_tuning = GridSearchCV(k_means, k_means_parameter, cv=5)
-    k_means_tuning.fit(coffeeData[flavorProfile])
-    y_pred = k_means_tuning.predict(coffeeData[flavorProfile])
-    plt.scatter(results[:,0], results[:,1], c=y_pred, s=1)
-    plt.savefig(mode + '-cluster-original.png', dpi=300)
-    plt.axis('off')
-    plt.show()
 
     k_means_parameter = {"n_clusters": [i + 1 for i in range(1, 10)]}
     k_means = KMeans(random_state=sd)
     k_means_tuning = GridSearchCV(k_means, k_means_parameter, cv=5)
     k_means_tuning.fit(results)
     y_pred = k_means_tuning.predict(results)
-    plt.scatter(results[:,0], results[:,1], c=y_pred, s=1)
-    plt.savefig(mode + '-cluster-project.png', dpi=300)
+    plt.scatter(results[:,0], results[:,1], c=y_pred)
+    plt.savefig(mode + '-std-cluster-original-add_attribute.png', dpi=300)
     plt.axis('off')
     plt.show()
 
-
+    k_means_parameter = {"n_clusters": [i + 1 for i in range(1, 10)]}
+    k_means = KMeans(random_state=sd)
+    k_means_tuning = GridSearchCV(k_means, k_means_parameter, cv=5)
+    k_means_tuning.fit(x_data_std)
+    y_pred = k_means_tuning.predict(x_data_std)
+    plt.scatter(results[:,0], results[:,1], c=y_pred)
+    plt.savefig(mode + '-std-cluster-project-add_attribute.png', dpi=300)
+    plt.axis('off')
+    plt.show()
 
 main()
